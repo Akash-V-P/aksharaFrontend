ef6f0cfe54229e86dead38a0d28f23f3b1ebf106
Akash-V-P <akashvp.dev@gmail.com>
2025-12-27 21:10:43 +0530

    handel 404 and add auth layout

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,10 @@
 export default function NotFound() {
-  return <h1 className="text-2xl font-bold">404 - Not Found</h1>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div>
+        <h1 className="text-center text-8xl font-bold">404</h1>
+        <p className="mt-2 text-4xl text-muted-foreground">Page not found</p>
+      </div>
+    </div>
+  );
 }
