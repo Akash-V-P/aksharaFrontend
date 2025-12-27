26acc0ea69e6bdad014b46e0736b251f559d6f98
Akash-V-P <akashvp.dev@gmail.com>
2025-12-27 21:11:24 +0530

    add 404 and auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,29 @@ import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Profile from "@/pages/Profile";
 import Protectedroutes from "./routes/ProtectedRoutes";
+import NotFound from "./pages/NotFound";
+import AuthLayout from "./layout/AuthLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* auth routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+
         <Route element={<MainLayout />}>
           {/* public routes */}
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
 
           {/* protected routes */}
           <Route element={<Protectedroutes />}>
-            <Route path="/profile" element={<Profile/>} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
-
         </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
