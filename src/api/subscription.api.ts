767a6c56de290af429223ffaafd7b71d40fdc1a5
Akash-V-P <akashvp.dev@gmail.com>
2026-01-30 18:01:19 +0530

    feat: add getFollowers and getFollowing API functions for subscription management

--- src/api/subscription.api.ts
+++ src/api/subscription.api.ts
@@ -4,3 +4,14 @@ export const toggleSubscription = async(channelId: string) => {
     const res = await api.post(`/subscription/c/${channelId}`);
     return res.data;
 }
+
+export const getFollowers = async ( channelId: string ) => {
+    const res = await api.get(`/subscription/c/getFollowers/${channelId}`);
+    return res.data.data;
+}
+
+export const getFollowing = async ( channelId: string ) => {
+    const res = await api.get(`/subscription/c/getFollowers/${channelId}`);
+    return res.data.data;
+
+}
